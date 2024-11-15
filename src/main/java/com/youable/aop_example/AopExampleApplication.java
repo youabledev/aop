56e632610d5fcfb02fb015ca75384297cd141f83
youable <youable.framios@gmail.com>
2024-11-15 16:02:35 +0900

    init proejct

---
+++ src/main/java/com/youable/aop_example/AopExampleApplication.java
@@ -0,0 +1,13 @@
+package com.youable.aop_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youable/aop_example/AopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youable.aop_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
